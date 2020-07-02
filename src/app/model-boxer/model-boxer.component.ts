fb3ad81216130cfa6acac164958fe02650e7a4b2
MasterTomNL <tomvanderlek@gmail.com>
2020-07-03 01:20:22 +0200

    2 files modified

--- src/app/model-boxer/model-boxer.component.ts
+++ src/app/model-boxer/model-boxer.component.ts
@@ -8,7 +8,17 @@ import * as THREE from 'three';
 })
 export class ModelBoxerComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    let renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+
+    var camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 );
+    camera.position.set( 0, 0, 100 );
+    camera.lookAt( 0, 0, 0 );
+
+    const scene = new THREE.Scene();
+  }
 
   ngOnInit() {
   }
